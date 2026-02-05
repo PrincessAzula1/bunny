6e261914c1042a7bf9cb067844c666644d8233c2
PrincessAzula1 <PrincessAzula1@users.noreply.github.com>
2026-02-05 09:23:29 +0000

    deploy: 738d8b94ce43c9844872c2e0c09ffc64413f3613

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2065491083"
+    serviceWorkerVersion: "1201852624"
   }
 });
