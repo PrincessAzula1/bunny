75cf3f637334030c78b1633ab5692cf06696c3e6
den max <xxxtenkills8888@gmail.com>
2026-02-10 14:56:50 +0300

    Fix: bubble2

--- lib/main.dart
+++ lib/main.dart
@@ -831,7 +831,7 @@ class _BunnyScreenState extends State<BunnyScreen> {
             bottom:
                 isMobileLandscape ? size.height * 0.12 : (isMobile ? 120 : 30),
             left: 0,
-            right: isMobileLandscape ? 0 : (isMobile ? 0 : -100),
+            right: isMobileLandscape ? 0 : (isMobile ? 0 : 0),
             child: Center(
               child: Stack(
                 alignment: Alignment.center,
